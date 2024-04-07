f4ab7e01a91cb2f27a11f93b2c28db24363778b9
sneaky-00-ninja <150247419+sneaky-00-ninja@users.noreply.github.com>
2024-04-07 10:45:20 +0200

    Update Build out a Diner Menu.js
    Improved the menu to now work with the breakfast and dinner options.

--- Build out a Diner Menu.js
+++ Build out a Diner Menu.js
@@ -3,10 +3,15 @@ var currentItem = null;
 var currentItemCost = 0;
 var ordered = [];
 var multiplier = 1;
+var whichMenu = null;
 
-const comments = ["Yummy.", "Nice.", "My dad ate that yesterday.", "That is a popular choice.", "I think I'll have that too."]; 
+const comments = ["Yummy.", "Nice.", "My dad ate that yesterday.", "That is a popular choice.", "Mmmm, I think I'll have that too."]; 
 
-const prices = new Map([
+const prices = new Map([  
+  ["toast", 1],
+  ["fruit", 2],
+  ["eggs", 3],
+  ["bacon", 4],
   ["frogs", 4],
   ["snails", 6],  
   ["soup", 8],
@@ -28,6 +33,7 @@ const waitressComment = () => {
 
 const entree = () => {
   currentItem = prompt("For ENTREE... frogs or snails or soup ?");
+  currentItem = currentItem.toLowerCase();
   if (currentItem === "frogs" || "snails" || "soup") {
     listen();
     waitressComment();  
@@ -39,6 +45,7 @@ const entree = () => {
 
 const side = () => {
   currentItem = prompt("For SIDE... chips or carrots or peas ?");
+ currentItem = currentItem.toLowerCase();
   if (currentItem === "chips" || "carrots" || "peas") {
     listen();
     waitressComment();  
@@ -48,37 +55,86 @@ const side = () => {
   }
 }
 
-alert("Welcome to McYummy's Restaurant. \n  ");
-var whichMenu = prompt(`Are you here for breakfast, lunch or dinner? \n \n  ENTER \n "b" for breakfast \n "l" for lunsh \n "d" for dinner.`);
-
-if (whichMenu === null) {
-  alert("Eat something!");
+const main = () => {
+  currentItem = prompt("For MAIN... chuleta or fish ?");
+ currentItem = currentItem.toLowerCase();
+  if (currentItem === "chuleta" || "fish") {
+    listen();
+    waitressComment();  
+  } else {
+    alert("No such item.");
+    currentItem = null;
+  }
 }
-console.log(whichMenu);
 
-if (whichMenu === "b") {
-  alert("Sorry, breakfast menu is no longer available! \n Try again tomorrow before 10am.");
-}
-
-if (whichMenu === "l") {
-  alert("Great, lunch is cheap for you here.");
+const dealChoice = () => {
   var deal = confirm("Would you like one entree and two side choices at regular cost? \n (and no main)");
   if (deal === true) {
     entree();
     side();
     side();
-    alert(`Total cost is... € ${totatCost}`)
-    
-
-  }
+    alert(`Total cost is... € ${totatCost}`);
+  } else {
+     var mainSelect = confirm("Would you like one main?");
+     if (mainSelect === true) {
+       main();      
+ //      alert(`Total cost is... € ${totatCost}`);
+     } else {
+       alert("Please come back when you are hungry.");       
+     }
+  }  
 }
 
-  if (whichMenu === "d") {
-    alert("Great, but please note that dinner costs 50% more than lunchtime prices.");
-    multiplier = 1.5;
-    // TODO enter item.
-    alert("Actually, sorry! Dinner doesn´t start till 7pm \n Please come back later.");   
-  }
+
+
+const breakfast = () => {
+  alert("Today's Breakfast menu... \n   \n toast €1  \n fruit €2 \n eggs €3 \n bacon €4 ");
+  currentItem = prompt("For BREAKFAST... toast or fruit or eggs or bacon?");
+  currentItem = currentItem.toLowerCase();
+  if (currentItem === "toast" || "fruit" || "eggs" || "bacon") {
+    listen();
+    waitressComment();  
+  } else {
+    alert("No such item.");
+    currentItem = null;
+  }  
+//  alert(`Total cost is... € ${totatCost}`);
+}
+
+const lunch = () => {
+  alert("Great, lunch is cheap for you here.");
+  alert("Today's Lunch menu... \n  \n ENTREES  \n frogs €8 \n snails €6  \n soup €4 \n  \n SIDES  \n chips €2 \n carrots €4 \n peas €6   \n \n MAINS  \n chuleta €46  \n fish €32 ");
+  dealChoice();
+}
+ 
+const dinner = () => {
+    alert("Great, but please note that dinner costs 50% more than lunchtime prices.");  
+    multiplier = 1.5;  
+    alert("Today's Dinner menu... \n  \n ENTREES  \n frogs €12 \n snails €9  \n soup €6 \n  \n SIDES  \n chips €3 \n carrots €6 \n peas €69  \n \n MAINS  \n chuleta €69  \n fish €48 ");
+    dealChoice();  
+}
 
 
 
+
+alert("Welcome to McYummy's Restaurant. \n  ");
+var whichMenu = prompt(`Are you here for breakfast, lunch or dinner? \n \n  ENTER \n "B" for breakfast \n "L" for lunsh \n "D" for dinner \n "N" for nothing.`);
+whichMenu = whichMenu.toLowerCase();
+
+console.log(whichMenu);
+
+if (whichMenu === "b") {
+  breakfast();
+}
+
+if (whichMenu === "l") {
+  lunch();
+}
+
+if (whichMenu === "d") {
+  dinner();
+}
+
+if (whichMenu === "n") {
+  alert("Please come back when you are hungry.");
+} 
