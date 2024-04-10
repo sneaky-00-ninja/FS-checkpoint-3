0dde400d6f4f570f2ede3838b54c9a1bf70ac552
sneaky-00-ninja <150247419+sneaky-00-ninja@users.noreply.github.com>
2024-04-10 18:23:43 +0200

    Update Build out a Diner Menu.js
    Time entry added. 
    
    Also a record of items added is now taken, and is displayed with the bill.

--- Build out a Diner Menu.js
+++ Build out a Diner Menu.js
@@ -4,6 +4,7 @@ var currentItemCost = 0;
 var ordered = [];
 var multiplier = 1;
 var whichMenu = null;
+var timeHour = "midnight";
 
 const comments = ["Yummy.", "Nice.", "My dad ate that yesterday.", "That is a popular choice.", "Mmmm, I think I'll have that too."]; 
 
@@ -18,14 +19,63 @@ const prices = new Map([
   ["chips", 2],  
   ["carrots", 4],
   ["peas", 6],  
-  ["chuleta", 46],
-  ["fish", 32],  
+  ["chuleta", 24],
+  ["fish", 18],  
 ]);
+
+
+
+const enterHour = () => {
+  timeHour = prompt(`What is the current Hour of the day, in 24:00 format? \n Please enter a value between 00 and 23 `);
+  console.log(timeHour);
+  //timeHour = Number(timeHour);
+  if (timeHour == null || timeHour == "") {
+    alert("You must enter a number!!!");
+    enterHour ();    
+  } else if (timeHour > 23) {
+    alert("You must enter a number less than 24 !!!");
+    enterHour ();
+  } else {
+    if (timeHour >= 0 && timeHour <= 23) {
+      alert(`The time is between ${timeHour}:00 and ${timeHour}:59 `)
+    } else {
+      alert("Please state the time! \n(My watch isnt working.)");      
+      enterHour ();
+    }    
+  }
+console.log("Hour is " + timeHour);
+}
+
+
+const brekLunchOrDinner = () => {
+  if (timeHour < 6) {
+    alert("We are closed. Please come back after 6am.");
+  } else if (timeHour < 11) {
+    alert("We are open for breakfast.");
+    breakfast();
+  } else if (timeHour < 17) {
+    alert("We are open for lunch.");
+    lunch();
+  } else if (timeHour < 22) {
+    alert("We are open for dinner.");
+    dinner();
+  } else if (timeHour >= 22) {
+    alert("Sorry, we are closed.");
+  }  
+}
+
+
+
+
+
 
 const listen = () => {
   currentItemCost = prices.get(currentItem)*multiplier; 
   totatCost = totatCost + currentItemCost;
-}
+  ordered.push(currentItem);
+}    
+  
+  
 const waitressComment = () => {
   alert(comments[(Math.floor(Math.random() * comments.length))]);  
   alert(`That costs € ${currentItemCost}`);
@@ -73,18 +123,17 @@ const dealChoice = () => {
     entree();
     side();
     side();
-    alert(`Total cost is... € ${totatCost}`);
+    alert(`Your order is... \n ${ordered} \n Total cost is... € ${totatCost}`);
   } else {
      var mainSelect = confirm("Would you like one main?");
      if (mainSelect === true) {
-       main();      
- //      alert(`Total cost is... € ${totatCost}`);
+       main();    
+      alert(`Your order is... \n ${ordered} \n Total cost is... € ${totatCost}`);
      } else {
        alert("Please come back when you are hungry.");       
      }
   }  
-}
-
+}  
 
 
 const breakfast = () => {
@@ -102,13 +151,13 @@ const breakfast = () => {
 }
 
 const lunch = () => {
-  alert("Great, lunch is cheap for you here.");
+  alert("This is great! lunch is cheap for you here.");
   alert("Today's Lunch menu... \n  \n ENTREES  \n frogs €8 \n snails €6  \n soup €4 \n  \n SIDES  \n chips €2 \n carrots €4 \n peas €6   \n \n MAINS  \n chuleta €46  \n fish €32 ");
   dealChoice();
 }
  
 const dinner = () => {
-    alert("Great, but please note that dinner costs 50% more than lunchtime prices.");  
+    alert("Please note that dinner costs 50% more than lunchtime prices.");  
     multiplier = 1.5;  
     alert("Today's Dinner menu... \n  \n ENTREES  \n frogs €12 \n snails €9  \n soup €6 \n  \n SIDES  \n chips €3 \n carrots €6 \n peas €69  \n \n MAINS  \n chuleta €69  \n fish €48 ");
     dealChoice();  
@@ -118,23 +167,5 @@ const dinner = () => {
 
 
 alert("Welcome to McYummy's Restaurant. \n  ");
-var whichMenu = prompt(`Are you here for breakfast, lunch or dinner? \n \n  ENTER \n "B" for breakfast \n "L" for lunsh \n "D" for dinner \n "N" for nothing.`);
-whichMenu = whichMenu.toLowerCase();
-
-console.log(whichMenu);
-
-if (whichMenu === "b") {
-  breakfast();
-}
-
-if (whichMenu === "l") {
-  lunch();
-}
-
-if (whichMenu === "d") {
-  dinner();
-}
-
-if (whichMenu === "n") {
-  alert("Please come back when you are hungry.");
-} 
+enterHour ();
+brekLunchOrDinner ();
