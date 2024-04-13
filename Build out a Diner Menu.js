5696c8591ded5b5713df5233f1a3d828a494082e
sneaky-00-ninja <150247419+sneaky-00-ninja@users.noreply.github.com>
2024-04-13 20:27:15 +0200

    Update Build out a Diner Menu.js
    Prices and pricing system revamped. 
    
    The menu is now complete and meets all requested requirements. 
    
    (except that no valid entries are still permitted, and they will spoil the final bill with a NaN value. )
     <so just the one issue still to fix>

--- Build out a Diner Menu.js
+++ Build out a Diner Menu.js
@@ -1,3 +1,5 @@
+var currentItem = "none";
+var orderOpen = true;
 var totatCost = 0;
 var currentItem = null;
 var currentItemCost = 0;
@@ -5,28 +7,109 @@ var ordered = [];
 var multiplier = 1;
 var whichMenu = null;
 var timeHour = "midnight";
+var deal = false;
+var currentMenu = [];
 
-const comments = ["Yummy.", "Nice.", "My dad ate that yesterday.", "That is a popular choice.", "Mmmm, I think I'll have that too."]; 
+const comments = ["Yummy.", "Nice!!!", "My dad had that yesterday.", "That is a popular choice.", "Mmmm, I think I'll have that too.", "I like that.", "I knew you'd choose that."]; 
 
 const prices = new Map([  
-  ["toast", 1],
-  ["fruit", 2],
-  ["eggs", 3],
-  ["bacon", 4],
-  ["frogs", 4],
-  ["snails", 6],  
-  ["soup", 8],
-  ["chips", 2],  
-  ["carrots", 4],
-  ["peas", 6],  
-  ["chuleta", 24],
-  ["fish", 18],  
+  	["toast", 1],
+ 	["fruit", 2],
+ 	["eggs", 3],
+ 	["bacon", 4],
+ 	["soup", 4],
+ 	["snails", 6],  
+ 	["frogs", 8],
+ 	["chips", 2],  
+	["carrots", 4],
+ 	["peas", 4],  
+	["chuleta", 24],
+	["fish", 18],  
+	["tea", 1], 
+	["coffee", 2], 
+	["juice", 2], 
+	["soda", 2], 
+	["wine", 4], 
+	["beer", 4],
+	[" ", 0],	
 ]);
 
+const brekMenu =[
+    {item: 'toast', cost: 1},
+    {item: 'fruit', cost: 2},
+    {item: 'eggs', cost: 3},
+    {item: 'bacon', cost: 4}
+];
+
+const brekDrinkMenu =[
+    {item: 'tea', cost: 1},
+    {item: 'coffee', cost: 2},
+    {item: 'juice', cost: 2}
+];
+
+const entreeMenu =[
+    {item: 'frogs', cost: 4},
+    {item: 'snails', cost: 6},
+    {item: 'soup', cost: 8}
+];
+
+const sideMenu =[
+    {item: 'chips', cost: 2},
+    {item: 'carrots', cost: 4},
+    {item: 'peas', cost: 6}
+];
+const mainMenu =[
+    {item: 'chuleta', cost: 24},
+    {item: 'fish', cost: 18},
+];
+
+const drinkMenu =[
+    {item: 'coffee', cost: 2},
+    {item: 'soda', cost: 2},
+    {item: 'wine', cost: 4},
+    {item: 'beer', cost: 4}
+];
+
+
+      // brekMenu      // alert(`Here is the breakfast menu: \n ${showBrekMenu}`);
+let showBrekMenu = brekMenu.map(function(e){
+  return `${e.item} ${e.cost}€` +"\n";
+});
+
+
+      // brekDrinks      // alert(`For breakfast drinks we have: \n ${showBrekDrinkMenu}`);
+let showBrekDrinkMenu = brekDrinkMenu.map(function(e){
+  return `${e.item} ${e.cost}€` +"\n";
+});
+
+
+      // entreeMenu      // alert(`The entrees are: \n ${ShowEntreeMenu}`);
+let ShowEntreeMenu = entreeMenu.map(function(e){
+  return `${e.item} ${e.cost}€` +"\n";
+});  
+  
+
+      // sideMenu      // alert(`The side dishes are: \n ${ShowSideMenu}`);
+let ShowSideMenu = sideMenu.map(function(e){
+  return `${e.item} ${e.cost}€` +"\n";
+});  
+  
+
+      // mainMenu      // alert(`The main dishe options are: \n ${ShowMainMenu}`);
+let ShowMainMenu = mainMenu.map(function(e){
+  return `${e.item} ${e.cost}€` +"\n";
+});
+
+
+      // drinkMenu      // alert(`What would you like to drink? Your choices are: \n ${ShowDrinkMenu}`);
+let ShowDrinkMenu = drinkMenu.map(function(e){
+  return `${e.item} ${e.cost}€` +"\n";
+});
 
 
 const enterHour = () => {
-  timeHour = prompt(`What is the current Hour of the day, in 24:00 format? \n Please enter a value between 00 and 23 `);
+	alert("Schedule... \n 6:00-10:59 breakfast \n 11:00-16:59 lunch \n 17:00-21:59 dinner")
+  timeHour = prompt(`What is the current Hour of the day, in 24:00 format? \n Please enter a value between 00 and 23`);
   console.log(timeHour);
   //timeHour = Number(timeHour);
   if (timeHour == null || timeHour == "") {
@@ -43,7 +126,7 @@ const enterHour = () => {
       enterHour ();
     }    
   }
-console.log("Hour is " + timeHour);
+//console.log("Hour is " + timeHour);
 }
 
 
@@ -60,112 +143,138 @@ const brekLunchOrDinner = () => {
     alert("We are open for dinner.");
     dinner();
   } else if (timeHour >= 22) {
-    alert("Sorry, we are closed.");
+    alert("Sorry, we are closed. Please return tomorrow at an earlier time.");
   }  
 }
 
-
-
-
-
-
-const listen = () => {
-  currentItemCost = prices.get(currentItem)*multiplier; 
-  totatCost = totatCost + currentItemCost;
-  ordered.push(currentItem);
-}    
-  
-  
-const waitressComment = () => {
-  alert(comments[(Math.floor(Math.random() * comments.length))]);  
-  alert(`That costs € ${currentItemCost}`);
-}
 
 const entree = () => {
   currentItem = prompt("For ENTREE... frogs or snails or soup ?");
   currentItem = currentItem.toLowerCase();
-  if (currentItem === "frogs" || "snails" || "soup") {
-    listen();
-    waitressComment();  
+  if (currentItem === "frogs" || currentItem === "snails" || currentItem === "soup") {
+	waitressComment();  
   } else {
-    alert("No such item.");
-    currentItem = null;
+		alert("No such item.");
+    	currentItem = " ";
   }
 }
+
 
 const side = () => {
   currentItem = prompt("For SIDE... chips or carrots or peas ?");
  currentItem = currentItem.toLowerCase();
-  if (currentItem === "chips" || "carrots" || "peas") {
-    listen();
+  if (currentItem === "chips" || currentItem === "carrots" || currentItem === "peas") {
     waitressComment();  
   } else {
-    alert("No such item.");
-    currentItem = null;
-  }
-}
-
-const main = () => {
-  currentItem = prompt("For MAIN... chuleta or fish ?");
- currentItem = currentItem.toLowerCase();
-  if (currentItem === "chuleta" || "fish") {
-    listen();
-    waitressComment();  
-  } else {
-    alert("No such item.");
-    currentItem = null;
+    	alert("No such item.");
+    	currentItem = " ";
   }
 }
 
 const dealChoice = () => {
-  var deal = confirm("Would you like one entree and two side choices at regular cost? \n (and no main)");
+  deal = confirm("Would you like one entree and two side choices at the regular lunchtime cost?");
   if (deal === true) {
+	multiplier = 1;
     entree();
     side();
     side();
-    alert(`Your order is... \n ${ordered} \n Total cost is... € ${totatCost}`);
-  } else {
-     var mainSelect = confirm("Would you like one main?");
-     if (mainSelect === true) {
-       main();    
-      alert(`Your order is... \n ${ordered} \n Total cost is... € ${totatCost}`);
-     } else {
-       alert("Please come back when you are hungry.");       
-     }
-  }  
+	multiplier = 1.5;
+    //alert(`Your order is... \n ${ordered} \n Total cost is... € ${totatCost}`);
+  } 
 }  
 
 
 const breakfast = () => {
-  alert("Today's Breakfast menu... \n   \n toast €1  \n fruit €2 \n eggs €3 \n bacon €4 ");
-  currentItem = prompt("For BREAKFAST... toast or fruit or eggs or bacon?");
-  currentItem = currentItem.toLowerCase();
-  if (currentItem === "toast" || "fruit" || "eggs" || "bacon") {
-    listen();
-    waitressComment();  
-  } else {
-    alert("No such item.");
-    currentItem = null;
-  }  
-//  alert(`Total cost is... € ${totatCost}`);
+	alert("what would you like to drink?");
+	currentMenu = showBrekDrinkMenu;
+	takeOrder();
+	alert("what would you like to eat?");
+	currentMenu = showBrekMenu;
+	takeOrder();
+	alert(`Your order is... \n ${ordered} \n Total cost is... € ${totatCost}`);
 }
+
 
 const lunch = () => {
-  alert("This is great! lunch is cheap for you here.");
-  alert("Today's Lunch menu... \n  \n ENTREES  \n frogs €8 \n snails €6  \n soup €4 \n  \n SIDES  \n chips €2 \n carrots €4 \n peas €6   \n \n MAINS  \n chuleta €46  \n fish €32 ");
-  dealChoice();
+	alert("what would you like to drink?");
+	currentMenu = ShowDrinkMenu;
+	takeOrder();
+	alert("For entree...");
+	currentMenu = ShowEntreeMenu;
+	takeOrder();
+	alert("for sides...");
+	currentMenu = ShowSideMenu;
+	takeOrder();	
+	alert("for main course...");
+	currentMenu = ShowMainMenu;
+	takeOrder();	
+	alert(`Your order is... \n ${ordered} \n Total cost is... € ${totatCost}`);
 }
- 
+
+
 const dinner = () => {
-    alert("Please note that dinner costs 50% more than lunchtime prices.");  
-    multiplier = 1.5;  
-    alert("Today's Dinner menu... \n  \n ENTREES  \n frogs €12 \n snails €9  \n soup €6 \n  \n SIDES  \n chips €3 \n carrots €6 \n peas €69  \n \n MAINS  \n chuleta €69  \n fish €48 ");
-    dealChoice();  
+	multiplier = 1.5;
+	alert("Please note that dinner items costs 50% more than lunchtime prices!!!");  
+	alert("what would you like to drink?");
+	currentMenu = ShowDrinkMenu;
+	takeOrder();
+	alert("what would you like to eat? ...");
+	dealChoice();
+	if (deal === false) {
+		alert("For entree...");
+		currentMenu = ShowEntreeMenu;
+		takeOrder();
+		alert("for sides...");
+		currentMenu = ShowSideMenu;
+		takeOrder();		
+	} 
+	alert("for main course...");
+	currentMenu = ShowMainMenu;
+	takeOrder();	
+	alert(`Your order is... \n ${ordered} \n Total cost is... € ${totatCost}`);
+}
+
+
+const waitressComment = () => {
+	currentItemCost = prices.get(currentItem)*multiplier; 
+	totatCost = totatCost + currentItemCost;
+	ordered.push(currentItem);
+	var currentComment = comments[(Math.floor(Math.random() * comments.length))];
+	alert(`${currentItem} , ${currentComment} `);
+		//alert(comments[(Math.floor(Math.random() * comments.length))]);  
+	alert(`That costs € ${currentItemCost}`);
 }
 
 
 
+const takeOrder = () => {
+	orderOpen = true;
+	while (orderOpen == true) {
+		//currentItem = prompt("What item do you choose now? \n enter END to finish this part of the order.");
+		currentItem = prompt(`What item do you choose now? \n ${currentMenu} \n \n (enter NO to finish this part of the order)`);
+		if (currentItem === null || currentItem === ""  ) {
+			alert("you must enter an item. \n (or 'NO' to continue.")
+			currentItem = "no entry";
+		} else {
+			currentItem = currentItem.toLowerCase();
+			if (currentItem === "no") {
+				orderOpen = false;
+				alert("Okay.");
+			} else {
+				waitressComment();
+				var more = confirm("Anything else?");
+				if (more === false){
+					orderOpen = false;
+				}
+			}
+		}
+			
+	}  // end of while loop. 
+}  // end of takeOrder function. 
 
-alert("Welcome to McYummy's Restaurant. \n  ");
-enterHour ();
-brekLunchOrDinner ();
+
+
+
+alert("Welcome to McYummy's Restaurant.");
+enterHour();
+brekLunchOrDinner();
